3955e386964f75f8ae117fc929c44f19f196dfa6
jonathan hamel <hamelj2991@outlook.com>
2019-05-05 01:21:56 -0700

    UX changes

--- Study_Buddy/wwwroot/js/site.js
+++ Study_Buddy/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+/* Open when someone clicks on the element */
+function openNav() {
+    document.getElementById("myNav").style.width = "50%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
