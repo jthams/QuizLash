d131dc874225318da9f51e17dc4d751ba61fd979
Jonathan T Hamel <hamelj2991@gmail.com>
2019-05-06 08:29:48 -0700

    old code

--- Study_Buddy/wwwroot/js/site.js
+++ Study_Buddy/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
