283fa66f167100caf174741c853beb088d5f5ad7
Jonathan T Hamel <hamelj2991@gmail.com>
2019-05-06 15:58:20 -0700

    Changed index styling

---
+++ Speech/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace Speech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
--- Study_Buddy/wwwroot/js/site.js
+++ Study_Buddy/wwwroot/js/site.js
@@ -5,10 +5,24 @@
 
 /* Open when someone clicks on the element */
 function openNav() {
-    document.getElementById("myNav").style.width = "50%";
+    var overlay = document.getElementById("myNav");
+    if (overlay.style.width === "50%") {
+        overlay.style.width = "0%";
+    } else {
+        overlay.style.display = "50%"
+    }
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+/*function openNav() {
+    var overlay = document.getElementById("myNav");
+    if (overlay.style.width === "50%") {
+        overlay.style.width = "0%";
+    } else {
+        overlay.style.display = "50%"
+    }
+}*/
